2baa908adfaa443acd8fbb8601cc0f58dd6847e6
rodaorm <spivakdm@gmail.com>
2020-04-12 23:17:55 +0300

    adafruit motor test

--- code/my_package/my_script.py
+++ code/my_package/my_script.py
@@ -1,5 +1,12 @@
 import os
+import RPi.GPIO
+import time
+from adafruit_motorki import MotorKit
 
 message = "Hello world, my name is %s" % os.environ['VEHICLE_NAME']
 print(message)
+kit = MotorKit()
 
+kit.motor1.throttle = 1.0
+time.sleep(0.5)
+kit.motor1.throttle = 0
